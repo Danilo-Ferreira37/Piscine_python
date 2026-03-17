ab4b3d16665219367b8564270f1b97079196c9d7
Danilo Osorio Nunes Ferreira <dosorio-@c1r3s11.42lisboa.com>
2026-03-17 21:37:50 +0000

    lol

--- py8/ex2/oracle.py
+++ py8/ex2/oracle.py
@@ -1,6 +1,11 @@
 import os, sys
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv()
+    mode = os.getenv("MATRIX_MODE")
+    db = os.getenv("DATABASE_URL")
+    
     print("\nORACLE STATUS: Reading the Matrix...\n")
     print("Configuration loaded:")
     print(f"Mode: ")
